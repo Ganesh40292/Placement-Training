ad8ef833ac8fe77c2bf503bbf74e26af43c0911d
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-10-25 10:32:33 +0530

    Add Array-to-ArrayList conversion example

---
+++ Array-to-ArrayList.java
@@ -0,0 +1,15 @@
+package hell;
+
+import java.util.*;
+
+class P3 
+{
+    public static void main(String[] args) 
+    {
+        String[] arr = {"A", "B", "C"};
+        List<String> list = Arrays.asList(arr);
+        System.out.println(list);
+    }
+}
+
+
