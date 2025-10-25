d859cb7ba69eb30c33bb3b48ef26177f715efdd7
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-10-25 10:43:02 +0530

    Create Sum of the Array.java

---
+++ Sum of the Array.java
@@ -0,0 +1,14 @@
+package hell;
+
+public class P6 
+{
+    public static void main(String[] args) 
+    {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for (int n : arr) 
+        	sum += n;
+        System.out.println("Sum = " + sum);
+    }
+}
+
