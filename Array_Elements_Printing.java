0de1621e7729d6573aa0299562fcd7cb88631995
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-10-25 10:14:56 +0530

    Implement array elements printing in Java

---
+++ Array_Elements_Printing.java
@@ -0,0 +1,15 @@
+package hell;
+
+public class P1 
+{
+	public static void main(String[] args) 
+	{
+	        String[] arr = {"Apple", "Banana", "Cherry"};
+	        for (String s : arr) 
+	        {
+	            System.out.println(s);
+	        }
+	 }
+}
+
+
